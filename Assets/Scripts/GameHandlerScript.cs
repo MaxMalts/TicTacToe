b0c350ce15cb62b35d9dbe37b3be51e8e88e1e05
MaxMalts <max-kurt@bk.ru>
2021-01-25 20:50:59 +0300

    Started. Created field and cells.

---
+++ Assets/Scripts/GameHandlerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameHandlerScript : MonoBehaviour
+{
+
+	private const int fieldSize = 3;
+
+	public GameObject cells;
+
+	private int[,] fieldState = new int[fieldSize, fieldSize];
+
+
+    void Start()
+    {
+		//cells.AddCellClickCallback(CellClickHandler);
+    }
+
+
+	void CellClickHandler(Vector2 pos)
+	{
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
