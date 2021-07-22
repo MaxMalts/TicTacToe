7e653b85ffa8e824f2ba0192dd2e2b80acc59326
MaxMalts <max-kurt@bk.ru>
2021-07-22 22:17:41 +0300

    Created singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+
+	private static T instance = null;
+	public static T Instance {
+		get {
+			if (instance == null) {
+				instance = FindObjectOfType<T>();
+
+				if (instance == null) {
+					GameObject newObject = new GameObject();
+					newObject.name = typeof(T).Name;
+					instance = newObject.AddComponent<T>();
+				}
+			}
+
+			return instance;
+		}
+	}
+
+	protected virtual void Awake() {
+		if (instance == null) {
+			instance = this as T;
+			Assert.IsNotNull(instance);
+			DontDestroyOnLoad(gameObject);
+
+		} else {
+			Destroy(gameObject);
+		}
+	}
+}
