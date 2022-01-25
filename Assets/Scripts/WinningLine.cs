54ab225acf8671cf8e550d9538b7732a348eb006
MaxMalts <max-kurt@bk.ru>
2022-01-26 02:46:06 +0300

    Animated winning line

--- Assets/Scripts/WinningLine.cs
+++ Assets/Scripts/WinningLine.cs
@@ -8,10 +8,34 @@ using UnityEngine.Assertions;
 [RequireComponent(typeof(LineRenderer))]
 public class WinningLine : MonoBehaviour {
 
+	[SerializeField] bool animate = true;
+	public bool Animate {
+		get {
+			return animate;
+		}
+
+		set {
+			animate = value;
+		}
+	}
+
+	[SerializeField] int animationDurationMs = 200;
+	public int AnimationDurationMs {
+		get {
+			return animationDurationMs;
+		}
+
+		set {
+			animationDurationMs = value;
+		}
+	}
+
 	// to place line ends further from the cell centers
 	const float stretchFactor = 1.2f;
 
 	LineRenderer lineRenderer;
+	Vector2 point1 = Vector2.zero;
+	Vector2 point2 = Vector2.zero;
 
 	CellsManager cellsManager;
 
@@ -23,20 +47,23 @@ public class WinningLine : MonoBehaviour {
 		Vector2 cellPos1 = cell1.ScenePos;
 		Vector2 cellPos2 = cell2.ScenePos;
 
-		Vector2 point1 = Vector2.LerpUnclamped(cellPos1, cellPos2, 1 - stretchFactor);
-		Vector2 point2 = Vector2.LerpUnclamped(cellPos1, cellPos2, stretchFactor);
+		point1 = Vector2.LerpUnclamped(cellPos1, cellPos2, 1 - stretchFactor);
+		point2 = Vector2.LerpUnclamped(cellPos1, cellPos2, stretchFactor);
 
 		Assert.IsTrue(lineRenderer.positionCount == 2,
 			"Number of points on LineRenderer component not 2.");
-
-		lineRenderer.SetPosition(0, point1);
-		lineRenderer.SetPosition(1, point2);
-
-		lineRenderer.enabled = true;
 	}
 
 	public void Show() {
-		lineRenderer.enabled = true;
+		if (animate) {
+			lineRenderer.enabled = true;
+			StartCoroutine(AnimateLine());
+
+		} else {
+			lineRenderer.SetPosition(0, point1);
+			lineRenderer.SetPosition(1, point2);
+			lineRenderer.enabled = true;
+		}
 	}
 
 	public void Hide() {
@@ -56,5 +83,22 @@ public class WinningLine : MonoBehaviour {
 	void Start() {
 		cellsManager = GameManager.Instance.CellsManager;
 		Assert.IsNotNull(cellsManager, "No cellsManager found.");
+	}
+
+	IEnumerator AnimateLine() {
+		float startTime = Time.time;
+		AnimationCurve curve =
+			AnimationCurve.EaseInOut(startTime, 0.0f, startTime + animationDurationMs / 1000.0f, 1.0f);
+
+		lineRenderer.SetPosition(0, point1);
+		lineRenderer.SetPosition(1, point1);
+
+		while ((Vector2)lineRenderer.GetPosition(1) != point2) {
+			float progress = curve.Evaluate(Time.time);
+			Vector2 curPoint = Vector2.Lerp(point1, point2, progress);
+			lineRenderer.SetPosition(1, curPoint);
+
+			yield return new WaitForEndOfFrame();
+		}
 	}
 }
