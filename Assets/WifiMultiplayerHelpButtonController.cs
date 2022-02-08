bc2ba92b7df8946249e3be37458467504ec9852a
MaxMalts <max-kurt@bk.ru>
2022-02-08 22:51:52 +0300

    Added help to WifiMultiplayer screen

---
+++ Assets/WifiMultiplayerHelpButtonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WifiMultiplayerHelpButtonController : MonoBehaviour {
+
+	public async void OnClick() {
+		ButtonPopupController popup = PopupsManager.ShowConfirmPopup(
+			"To play with your friend over the network, connect with your friend to " +
+			"the same WiFi or hotspot and click on different signs.");
+
+		await popup.WaitForClickOrCloseAsync();
+		popup.Close();
+	}
+}
